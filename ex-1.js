5e09061903c7c8de4be57d71c493ead3a0da775d
BBTC CTBB <enterpannet@gmail.com>
2023-12-15 01:01:18 +0700

    ex1-3

--- ex-1.js
+++ ex-1.js
@@ -1,3 +1,27 @@
-class EmailNotification { }
+class EmailNotification {
+  constructor(notificationId, createdTime, content, receiver) {
+    this.notificationId = notificationId;
+    this.createdTime = createdTime;
+    this.content = content;
+    this.receiver = receiver;
+  }
+  send(email) {
+    console.log(`Notification has been sent to ${email}`);
+  }
+}
 
-class SMSNotification { }
+class SMSNotification {
+  constructor(notificationId, createdTime, content, phoneNumber) {
+    this.notificationId = notificationId;
+    this.createdTime = createdTime;
+    this.content = content;
+    this.phoneNumber = phoneNumber;
+  }
+  send() {
+    console.log(`Notification has been sent to ${this.phoneNumber}`);
+  }
+}
+let e2mailNotification=new EmailNotification("1234","12.00","Tong","kiattisak")
+let s2MSNotification=new SMSNotification("321","12.30","สวัสดี","0987654321")
+e2mailNotification.send("email@gmail.com")
+s2MSNotification.send()
--- ex-2.js
+++ ex-2.js
@@ -1,5 +1,31 @@
-class Notification { }
+class Notification {
+  onstructor(notificationId, createdTime, content, receiver) {
+    this.notificationId = notificationId;
+    this.createdTime = createdTime;
+    this.content = content;
+    this.receiver = receiver;
+  }
+  send() {
+    console.log(`Notification has been sent to ${this.receiver}`);
+  }
+}
+class EmailNotification extends Notification {
+  constructor(notificationId, createdTime, content, receiver) {
+    super(notificationId, createdTime, content);
+    this.receiver = receiver;
+  }
+}
 
-class EmailNotification { }
-
-class SMSNotification { }
+class SMSNotification extends Notification{
+  constructor(notificationId, createdTime, content, phoneNumber) {
+    super(notificationId, createdTime, content);
+    this.phoneNumber = phoneNumber;
+  }
+  send() {
+    console.log(`Notification has been sent to ${this.phoneNumber}`);
+  }
+}
+let E1mailNotification=new EmailNotification("1234","12.00","Tong","kiattisak")
+let S1MSNotification=new SMSNotification("321","12.30","สวัสดี","0987654321")
+E1mailNotification.send()
+S1MSNotification.send()
